1841c1ef9c13fc6b32586ae3646fc77982c0a3b4
UltraHunter2007 <abdilaipovumar2007@gmail.com>
2023-03-17 12:01:35 +0600

    DZ2_Umar_23-03

---
+++ app.js
@@ -0,0 +1,36 @@
+const smallSquare = document.querySelector('.box');
+const radius = 210;
+const centerX = 237;
+const centerY = 237;
+let angle = 0;
+
+function move() {
+    const x = centerX + radius * Math.cos(angle);
+    const y = centerY + radius * Math.sin(angle);
+
+    smallSquare.style.top = y + 'px';
+    smallSquare.style.left = x + 'px';
+
+    angle += Math.PI / 180;
+
+    setTimeout(move, 10)
+}
+
+move()
+
+const counterElement = document.getElementById('counter');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let counter = 0;
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+        counter++;
+        counterElement.textContent = counter;
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+});
